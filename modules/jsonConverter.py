47117b6aec865da568cdd2dbc38aa4591921a7eb
azhou5211 <32046010+azhou5211@users.noreply.github.com>
2021-02-20 13:48:51 -0800

    modified jsonCoverter.py. Added a tests folder for unit testing. Added __init__.py files to modules and tests folder.

---
+++ modules/jsonConverter.py
@@ -0,0 +1,32 @@
+import pandas as pd
+
+
+def boi_convert_to_json(df):
+    """
+    Takes in dataframe object and converts it to a JSON object.
+    Returns the JSON object
+
+    :param df:
+    :return: json object
+    """
+
+    return df.to_json(orient="split")
+
+
+def boi_convert_to_df(json):
+    """
+
+    :param json:
+    :return: pandas DataFrame object
+    """
+
+    return pd.read_json(json_obj, orient="split")
+
+'''
+if __name__ == '__main__':
+    dataframe = pd.DataFrame({"a": [1, 2, 3], "b": [9, 8, 7], "c": [10, 20, 30]})
+    json_obj = boi_convert_to_json(dataframe)
+    print(json_obj)
+
+    print(boi_convert_to_df(json_obj))
+'''
