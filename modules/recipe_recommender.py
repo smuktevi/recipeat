0d7565763bba068138702d35465ed7e7d8b40484
louw <louw@uw.edu>
2021-03-06 18:02:14 -0800

    test_nutrition_requirements

--- modules/recipe_recommender.py
+++ modules/recipe_recommender.py
@@ -1,20 +1,17 @@
 import requests
 import pandas as pd
 from constants import *
-from bag_of_ingredients import BagOfIngredients
-
 
 
 class RecipeRecommender:
-    def __init__(self, boi:BagOfIngredients=None, preferences:dict = None):
-        self.boi = boi
-
-    def search_recipes(self, ingredients:list=[], nutritional_req:dict={}, diet:str="", intolerances:str=""):
+    def search_recipes(self, ingredients: list = [], nutritional_req: dict = {}, diet: str = "", intolerances: str = ""):
         search_recipes_url = "https://api.spoonacular.com/recipes/complexSearch"
         result_option_url = 'instructionsRequired=true&ignorePantry=true&sort=min-missing-ingredients&number=15&limitLicense=true'
-        preferences_url = 'diet={diet}&intolerances={intolerances}'.format(diet=diet,intolerances=intolerances)
+        preferences_url = 'diet={diet}&intolerances={intolerances}'.format(
+            diet=diet, intolerances=intolerances)
         ingredients_url = 'includeIngredients=' + ','.join(ingredients)
-        nutr_url = '&'.join("{!s}={!r}".format(key,val) for (key,val) in nutritional_req.items())
+        nutr_url = '&'.join("{!s}={!r}".format(key, val)
+                            for (key, val) in nutritional_req.items())
 
         search_url = "{search}?{apikey}&{result_options}&{ingredients}&{nutrition}&{preferences}".format(
             search=search_recipes_url,
@@ -24,29 +21,34 @@ class RecipeRecommender:
             nutrition=nutr_url,
             preferences=preferences_url)
 
-        payload={}
+        payload = {}
         headers = {
             'Cookie': '__cfduid=d443da310537f29e03b78e744720641111613622052'
         }
 
         recipes = []
 
-        search_response = requests.request("GET", search_url, headers=headers, data=payload)
+        search_response = requests.request(
+            "GET", search_url, headers=headers, data=payload)
         search_results = pd.DataFrame(search_response.json()["results"])
         if not search_results.empty:
             search_results = search_results[["id", "image", "title"]]
             # print(search_results)
             get_bulk_recipe_info_url = 'https://api.spoonacular.com/recipes/informationBulk'
-            recipe_id_url = 'ids=' + ','.join(str(id) for id in list(search_results["id"]))
+            recipe_id_url = 'ids=' + ','.join(str(id)
+                                              for id in list(search_results["id"]))
             recipe_info_url = "{get_recipe}?{apikey}&{recipe_ids}".format(
                 get_recipe=get_bulk_recipe_info_url,
                 apikey=apikey6,
                 recipe_ids=recipe_id_url)
 
-            source_response = requests.request("GET", recipe_info_url, headers=headers, data=payload)
-            source_results = pd.DataFrame(source_response.json())[["id", "sourceUrl"]]
+            source_response = requests.request(
+                "GET", recipe_info_url, headers=headers, data=payload)
+            source_results = pd.DataFrame(source_response.json())[
+                ["id", "sourceUrl"]]
 
-            results = search_results.join(source_results.set_index("id"), on="id", how='left', rsuffix='right')[["id", "title", "sourceUrl", "image"]]
+            results = search_results.join(source_results.set_index(
+                "id"), on="id", how='left', rsuffix='right')[["id", "title", "sourceUrl", "image"]]
             # print(results)
             for index, recipe in results.iterrows():
                 recipes.append(Recipe(
@@ -54,18 +56,19 @@ class RecipeRecommender:
                     recipe["title"],
                     recipe["sourceUrl"],
                     recipe["image"],
-                    str(RecipeRecommender.get_recipe_info(recipe["id"])),
-                    RecipeRecommender.recipe_to_ingredients(recipe["id"])))
-        
+                    str(self.get_recipe_info(recipe["id"])),
+                    self.recipe_to_ingredients(recipe["id"])))
+
         return recipes
 
     def recipe_to_ingredients(recipe_id):
-        request_url = 'https://api.spoonacular.com/recipes/{}/ingredientWidget.json?'.format(recipe_id)
+        request_url = 'https://api.spoonacular.com/recipes/{}/ingredientWidget.json?'.format(
+            recipe_id)
 
         ingredients = []
 
         url = request_url + apikey6
-        payload={}
+        payload = {}
         headers = {
             'Cookie': '__cfduid=dff952ebbf9c020c4f07c314e6bcb9c711613423774'
         }
@@ -75,18 +78,19 @@ class RecipeRecommender:
         # print(response.json()["status"] == 404)
         # print(type(results))
 
-
         for index, ingredient in results.iterrows():
-            ingredients.append(Ingredient(ingredient_full=str(ingredient['amount.metric.value']) + " " + ingredient["amount.metric.unit"] + " " + ingredient["name"], ingredient_name=ingredient["name"], amount=ingredient["amount.metric.value"], units=ingredient["amount.metric.unit"]))
+            ingredients.append(Ingredient(ingredient_full=str(ingredient['amount.metric.value']) + " " + ingredient["amount.metric.unit"] + " " +
+                                          ingredient["name"], ingredient_name=ingredient["name"], amount=ingredient["amount.metric.value"], units=ingredient["amount.metric.unit"]))
 
         return ingredients
-        #return list of Ingredient
+        # return list of Ingredient
 
     def get_recipe_info(recipe_id):
-        request_url = 'https://api.spoonacular.com/recipes/{}/analyzedInstructions?'.format(recipe_id)
+        request_url = 'https://api.spoonacular.com/recipes/{}/analyzedInstructions?'.format(
+            recipe_id)
 
         url = request_url + apikey6
-        payload={}
+        payload = {}
         headers = {
             'Cookie': '__cfduid=dff952ebbf9c020c4f07c314e6bcb9c711613423774'
         }
@@ -95,26 +99,24 @@ class RecipeRecommender:
 
         return response.json()
 
-    def recommend_from_boi(self):
-        return self.search_recipes(self.boi.get_boi())
-
-# if __name__ == '__main__':
-#     #test code
-#     nutrients = {
-#     'minCarbs': 1,
-#     'maxCarbs': 100,
-#     'minProtein': 1,
-#     'maxProtein': 100,
-#     'minCalories': 100,
-#     'maxCalories': 1000,
-#     'minFat': 1,
-#     'maxFat': 100
-#     }
-#     ingredients = ['chicken','potatoes']
-#     diet = 'vegetarian'
-#     intolerances='dairy'
-#     for recipe in RecipeRecommender.search_recipes(ingredients=ingredients, nutritional_req=nutrients):
-#         print(recipe)
+if __name__ == '__main__':
+    #test code
+    nutrients = {
+    'minCarbs': 1,
+    'maxCarbs': 100,
+    'minProtein': 1,
+    'maxProtein': 100,
+    'minCalories': 100,
+    'maxCalories': 1000,
+    'minFat': 1,
+    'maxFat': 100
+    }
+    ingredients = ['chicken','potatoes']
+    diet = 'vegetarian'
+    intolerances='dairy'
+    rr = RecipeRecommender()
+    for recipe in rr.search_recipes(ingredients=ingredients, nutritional_req=nutrients):
+        print(recipe)
 
 #     print(RecipeRecommender.search_recipes(ingredients,nutrients))
 #     print(RecipeRecommender.get_recipe_info('fake'))
--- tests/test_recipe_recommender.py
+++ tests/test_recipe_recommender.py
@@ -1,24 +1,76 @@
 import unittest
-from .modules import RecipeRecommender
+import requests
+import pandas as pd
+from modules.constants import *
+from modules.recipe_recommender import *
+
 
 class Test_Search_Recipes(unittest.TestCase):
     # test if recipes returned meet the nutritional requirements
     def test_nutrition_requirements(self):
+        nutrients = {
+            'minCarbs': 1,
+            'maxCarbs': 100,
+            'minProtein': 1,
+            'maxProtein': 100,
+            'minCalories': 100,
+            'maxCalories': 1000,
+            'minFat': 1,
+            'maxFat': 100
+        }
+        rr = RecipeRecommender()
+        recipes = rr.search_recipes(nutritional_req=nutrients)
 
-    # test if recipes returned include at least one of the input ingredients
-    def test_ingredient_requirements(self):
+        payload = {}
+        headers = {
+            'Cookie': '__cfduid=d443da310537f29e03b78e744720641111613622052'
+        }
+        slicer = slice(0, -1, 1)
+        for recipe in recipes:
+            get_nutrition_url = "https://api.spoonacular.com/recipes/{id}/nutritionWidget.json?{apikey}".format(
+                id=recipe.recipe_id, apikey=apikey6)
 
-    # test if the recipes returned meet diet requirements
-    def test_diet_requirements(self):
+            response = requests.request(
+                "GET", get_nutrition_url, headers=headers, data=payload)
 
-    # test if the recipes returned meet intolerance requirements
-    def test_intolerances_requirements(self):
+            recipeCalories = int(response["calories"])
+            recipeCarbs = int(response["carbs"][slicer])
+            recipeProtein = int(response["protein"][slicer])
+            recipeFat = int(response["fat"][slicer])
 
-class Test_Recipe_to_Ingredients(unittest.TestCase):
-    # 
+            self.assertGreaterEqual(
+                nutrients["maxCalories"], recipeCalories, "Recipe calories > maxCalories")
+            self.assertGreaterEqual(
+                nutrients["maxCarbs"], recipeCarbs, "Recipe carbs > maxCarbs")
+            self.assertGreaterEqual(
+                nutrients["maxProtein"], recipeProtein, "Recipe protein > maxProtein")
+            self.assertGreaterEqual(
+                nutrients["maxFat"], recipeFat, "Recipe fat > maxFat")
 
-class Test_Get_Recipe_Info(unittest.TestCase):
-    #
+            self.assertLessEqual(
+                nutrients["minCalories"], recipeCalories, "Recipe calories < minCalories")
+            self.assertLessEqual(
+                nutrients["minCarbs"], recipeCarbs, "Recipe carbs < minCarbs")
+            self.assertLessEqual(
+                nutrients["minProtein"], recipeProtein, "Recipe protein < minProtein")
+            self.assertLessEqual(
+                nutrients["minFat"], recipeFat, "Recipe fat < minFat")
 
-class Test_Recommend_From_Boi(unittest.TestCase):
-    #
+#     # test if recipes returned include at least one of the input ingredients
+#     def test_ingredient_requirements(self):
+
+
+#     # test if the recipes returned meet diet requirements
+#     def test_diet_requirements(self):
+
+#     # test if the recipes returned meet intolerance requirements
+#     def test_intolerances_requirements(self):
+
+# class Test_Recipe_to_Ingredients(unittest.TestCase):
+#     # test if recipe is valid/raises appropriate exception with recipe_id is not valid
+
+# class Test_Get_Recipe_Info(unittest.TestCase):
+#     # test if recipe is valid/raises appropriate exception with recipe_id is not valid
+
+if __name__ == '__main__':
+    unittest.main()
