731fe90ad480e12f47a546ed346b6cf867d4715d
geiercc <geiercc@uw.com>
2021-03-07 19:58:12 -0800

    Updating files to be pep8 compliant

--- modules/comparator.py
+++ modules/comparator.py
@@ -8,6 +8,7 @@ import requests
 from IPython.core.display import display, HTML
 from .constants import *
 
+
 class Compare:
     def nutrient_compare(self, recipes):
         """
@@ -16,15 +17,15 @@ class Compare:
         Args:
             recipes: list of recipe objects
 
-        Returns: list of HTML responses with nutrition information graphics   
+        Returns: list of HTML responses with nutrition information graphics
         """
         nutr_html_list = []
         for recipe in recipes:
-            url = "https://api.spoonacular.com/recipes/{id}/nutritionWidget?{apikey}&defaultCss=true".format(id=recipe.recipe_id,apikey=apikey6)    
-            payload={}
+            url = "https://api.spoonacular.com/recipes/{id}/nutritionWidget?{apikey}&defaultCss=true".format(id=recipe.recipe_id, apikey=apikey6)
+            payload = {}
             headers = {
                 'Cookie': '__cfduid=d443da310537f29e03b78e744720641111613622052'
-                    }  
+                    }
             response = requests.request("GET", url, headers=headers, data=payload)
             nutr_html_list.append('<div class="header"><h3>{recipe_name}</h3></div>'.format(recipe_name=recipe.recipe_name) + response.text)
         return nutr_html_list
@@ -36,21 +37,15 @@ class Compare:
         Args:
             recipes: list of recipe objects
 
-        Returns: list of HTML responses with ingredient graphics   
+        Returns: list of HTML responses with ingredient graphics
         """
         ingrd_html_list = []
-        for recipe in recipes: 
-            url = "https://api.spoonacular.com/recipes/{id}/ingredientWidget?{apikey}&defaultCss=true".format(id=recipe.recipe_id,apikey=apikey6)    
-            payload={}
+        for recipe in recipes:
+            url = "https://api.spoonacular.com/recipes/{id}/ingredientWidget?{apikey}&defaultCss=true".format(id=recipe.recipe_id, apikey=apikey6)
+            payload = {}
             headers = {
                 'Cookie': '__cfduid=d443da310537f29e03b78e744720641111613622052'
-                    }  
-            response = requests.request("GET", url, headers=headers, data=payload)   
+                    }
+            response = requests.request("GET", url, headers=headers, data=payload)
             ingrd_html_list.append('<div class="header"><h3>{recipe_name}</h3></div>'.format(recipe_name=recipe.recipe_name) + response.text)
         return ingrd_html_list
-
-
-
-
-
-
--- tests/test_comparator.py
+++ tests/test_comparator.py
@@ -1,6 +1,7 @@
-import comparator as cp
+import .comparator as cp
 import unittest
 import pandas as pd
+
 
 class testComparator(unittest.TestCase):
     def test_output_length(self):
@@ -18,21 +19,22 @@ class testComparator(unittest.TestCase):
             'minFat': 1,
             'maxFat': 100
             }
-        ingredients = ['chicken','potatoes']
+        ingredients = ['chicken', 'potatoes']
         recipe_list = RecipeRecommender.search_recipes(ingredients=ingredients, nutritional_req=nutrients)
         self.assertEqual(len(recipe_list), len(cp.nutrient_compare(recipe_list)))
         self.assertEqual(len(recipe_list), len(cp.ingredient_compare(recipe_list)))
-        
+
     def test_html_nutrient(self):
         """
         tests that the HTML format is correct HTML format for nutrient_compare output
 
-        Returns:  
+        Returns:
         """
-            
+        #self.assertTrue( )
+
     def test_html_ingredients(self):
         """
         tests that the HTML format is correct HTML format for ingredrient_compare output
 
-        Returns: 
-        """        
+        Returns:
+        """
