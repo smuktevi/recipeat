43d45663f7e9311f3f5568740cb15f9942d10795
azhou5211 <ajz55@uw.edu>
2021-03-13 13:50:53 -0800

    forms are flake8 standard and have pydocs

--- app/forms/register_form.py
+++ app/forms/register_form.py
@@ -1,5 +1,6 @@
 from flask_wtf import FlaskForm
-from wtforms import *
+from wtforms import StringField, PasswordField, IntegerField, RadioField, \
+    SubmitField
 from wtforms.validators import DataRequired
 
 
